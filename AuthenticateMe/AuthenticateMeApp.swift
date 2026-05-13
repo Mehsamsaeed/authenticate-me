771d5ff7616b9be74937fcf445de97c3756c8f53
Mehsam Saeed <mehsam.saeed@systemsltd.com>
2026-05-13 21:41:40 +0500

    Initial Commit

---
+++ AuthenticateMe/AuthenticateMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthenticateMeApp.swift
+//  AuthenticateMe
+//
+//  Created by Mehsam Saeed on 13/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct AuthenticateMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
